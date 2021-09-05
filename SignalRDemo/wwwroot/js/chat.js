6e42c73e7238fe42c60763d034e0c059ccbf5f11
karakurtsule <86284669+karakurtsule@users.noreply.github.com>
2021-09-05 16:43:27 +0300

    Chat App

--- SignalRDemo/wwwroot/js/chat.js
+++ SignalRDemo/wwwroot/js/chat.js
@@ -6,23 +6,23 @@
 
     connection.on("ReceiveMessage", function (user, message) {
         var li = document.createElement("li");
-        document.getElementById("messagesList").appendChild(li);
+        document.querySelector("#messagesList").appendChild(li);
         li.textContent = `${user} says ${message}`;
     });
 
     connection.start().then(function () {
-        document.getElementById("sendButton").disabled = false;
+        document.querySelector("#sendButton").disabled = false;
     }).catch(function (err) {
         return console.error(err.toString());
     });
 
-    document.getElementById("sendButton").addEventListener("click", function (event) {
+document.querySelector("#sendButton").addEventListener("click", function (e) {
         var user = document.getElementById("userInput").value;
         var message = document.getElementById("messageInput").value;
         connection.invoke("SendMessage", user, message).catch(function (err) {
             return console.error(err.toString());
         });
-        event.preventDefault();
+        e.preventDefault();
     });
     
 
